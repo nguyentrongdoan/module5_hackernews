8ea101e92c1d21cacadbcd633a5df2f3056f340d
nguyentrongdoan <nguyendoan206@gmail.com>
2020-07-03 16:37:50 +0700

    hackernews

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hackernews';
+  title: string;
+  url: string;
+  // title = 'hackernews';
+  // article = {
+  //   title: 'news',
+  //   url: 'https://vnexpress.net/'
+  // };
+  // updateArticle(){
+  //   this.article.title = (document.getElementById('article-title')as HTMLInputElement).value;
+  //   this.article.url = (document.getElementById('article-url')as HTMLInputElement).value;
+  // }
+  articles = [{
+    title: 'youtube',
+    url: 'https://www.youtube.com/watch?v=sXI8DlZ5ktI&list=PLkY6Xj8Sg8-uBQaBU8wMLo2CrFkE-9VIZ&index=17'
+  },
+    {
+      title: 'angular tutorial',
+      url: 'https://www.javatpoint.com/angular-8-ngif-directive'
+    }
+    ];
+  addArticle() {
+    this.articles.push({title: this.title, url: this.url});
+  }
 }
